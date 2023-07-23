49212261323a9ccb6e62bb549fc7b35eb1d8819f
Saffr3n <saffr3n@gmail.com>
2023-07-23 21:19:08 +0300

    Fix index controller for new passport logic

--- controllers/index.js
+++ controllers/index.js
@@ -1,31 +1,22 @@
 const passport = require('passport');
 const createError = require('http-errors');
-const User = require('../models/user');
 
 exports.signin = (req, res, next) => {
   passport.authenticate('json', async (err, user) => {
-    if (err) {
+    if (err) return next(err);
+
+    if (!user) {
+      const err = createError(400, 'Incorrect email or password');
       return next(err);
     }
 
-    if (!user) {
-      return next(createError(400, 'Incorrect email or password'));
-    }
-
-    const _user = await User.findById(user)
-      .select('role')
-      .exec()
-      .catch((err) => next(err));
-
     req.login(user, (err) => {
-      if (err) {
-        return next(err);
-      }
+      if (err) return next(err);
 
       res.status(200).json({
         status: 200,
         message: 'Signed in',
-        user: _user
+        session: req.user
       });
     });
   })(req, res, next);
@@ -33,26 +24,20 @@ exports.signin = (req, res, next) => {
 
 exports.signout = (req, res, next) => {
   req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
+    if (err) return next(err);
 
-    res.status(401).json({
-      status: 401,
-      message: 'Signed out'
+    res.status(200).json({
+      status: 200,
+      message: 'Signed out',
+      session: null
     });
   });
 };
 
-exports.session = async (req, res, next) => {
-  const user = await User.findById(req.session.passport.user)
-    .select('role')
-    .exec()
-    .catch((err) => next(err));
-
+exports.session = async (req, res) => {
   res.status(200).json({
     status: 200,
     message: 'Authorized',
-    user
+    session: req.user
   });
 };
