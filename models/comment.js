bc15c564b613d32fddbdf740843fda27f4d1ecf4
Saffr3n <saffr3n@gmail.com>
2023-08-04 18:30:03 +0300

    Edit ticket.js and comment.js models 1. add pre save and pre deleteOne hooks 2. remove unneeded virtuals

--- models/comment.js
+++ models/comment.js
@@ -2,21 +2,49 @@ const mongoose = require('mongoose');
 
 const CommentSchema = new mongoose.Schema({
   content: { type: String, required: true },
-  created: { type: Date, required: true, default: Date.now },
-  ticket: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Ticket',
-    required: true
-  },
-  submitter: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  }
+  ticket: { type: mongoose.Schema.Types.ObjectId, ref: 'Ticket', required: true },
+  submitter: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  created: { type: Date, default: Date.now }
 });
+CommentSchema.pre('save', async function () {
+  const comment = await this.populate('submitter ticket').catch((err) => {
+    throw err;
+  });
+  const { ticket, submitter } = comment;
 
-CommentSchema.virtual('url').get(function () {
-  return `/comments/${this.id}`;
+  await ticket
+    .updateOne({ $push: { comments: comment.id } })
+    .exec()
+    .catch((err) => {
+      throw err;
+    });
+
+  await submitter
+    .updateOne({ $push: { comments: comment.id } })
+    .exec()
+    .catch((err) => {
+      throw err;
+    });
+});
+CommentSchema.pre('deleteOne', { document: true, query: false }, async function () {
+  const comment = await this.populate('ticket submitter').catch((err) => {
+    throw err;
+  });
+  const { ticket, submitter } = comment;
+
+  await ticket
+    .updateOne({ $pull: { comments: comment.id } })
+    .exec()
+    .catch((err) => {
+      throw err;
+    });
+
+  await submitter
+    .updateOne({ $pull: { comments: comment.id } })
+    .exec()
+    .catch((err) => {
+      throw err;
+    });
 });
 
 module.exports = mongoose.model('Comment', CommentSchema);
--- models/ticket.js
+++ models/ticket.js
@@ -3,24 +3,45 @@ const mongoose = require('mongoose');
 const TicketSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  status: { type: Boolean, required: true, default: true },
-  created: { type: Date, required: true, default: Date.now },
-  project: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Project',
-    required: true
-  },
-  submitter: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
+  status: { type: Boolean, default: true },
+  project: { type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true },
+  submitter: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   devs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+  created: { type: Date, default: Date.now }
 });
+TicketSchema.pre('save', async function () {
+  const ticket = await this.populate('project submitter').catch((err) => {
+    throw err;
+  });
+  const { project, submitter } = ticket;
 
-TicketSchema.virtual('url').get(function () {
-  return `/tickets/${this.id}`;
+  await project
+    .updateOne({ $push: { tickets: ticket.id } })
+    .exec()
+    .catch((err) => {
+      throw err;
+    });
+
+  await submitter
+    .updateOne({ $push: { tickets: ticket.id } })
+    .exec()
+    .catch((err) => {
+      throw err;
+    });
+});
+TicketSchema.pre('deleteOne', { document: true, query: false }, async function () {
+  const ticket = await this.populate('project submitter devs comments').catch((err) => {
+    throw err;
+  });
+  const { project, submitter, devs, comments } = ticket;
+
+  const promises = [];
+  promises.push(...[project, submitter, ...devs].map((doc) => doc.updateOne({ $pull: { tickets: ticket.id } }).exec()), ...comments.map((comment) => comment.deleteOne()));
+
+  await Promise.all(promises).catch((err) => {
+    throw err;
+  });
 });
 
 module.exports = mongoose.model('Ticket', TicketSchema);
