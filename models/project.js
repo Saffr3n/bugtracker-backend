b6470d423a070c772c0bbda6427406b336a58410
Saffr3n <saffr3n@gmail.com>
2023-08-04 18:21:23 +0300

    Edit project.js model 1. add pre save and pre deleteOne hooks 2. remove unneeded virtual

--- models/project.js
+++ models/project.js
@@ -3,18 +3,37 @@ const mongoose = require('mongoose');
 const ProjectSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
-  created: { type: Date, required: true, default: Date.now },
-  manager: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
+  manager: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  tickets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Ticket' }]
+  tickets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Ticket' }],
+  created: { type: Date, default: Date.now }
 });
+ProjectSchema.pre('save', async function () {
+  const project = await this.populate('manager').catch((err) => {
+    throw err;
+  });
+  const { manager } = project;
 
-ProjectSchema.virtual('url').get(function () {
-  return `/projects/${this.id}`;
+  await manager
+    .updateOne({ $push: { projects: project.id } })
+    .exec()
+    .catch((err) => {
+      throw err;
+    });
+});
+ProjectSchema.pre('deleteOne', { document: true, query: false }, async function () {
+  const project = await this.populate('manager users tickets').catch((err) => {
+    throw err;
+  });
+  const { manager, users, tickets } = project;
+  users.push(manager);
+
+  const promises = [];
+  promises.push(...users.map((user) => user.updateOne({ $pull: { projects: project.id } }).exec()), ...tickets.map((ticket) => ticket.deleteOne()));
+
+  await Promise.all(promises).catch((err) => {
+    throw err;
+  });
 });
 
 module.exports = mongoose.model('Project', ProjectSchema);
